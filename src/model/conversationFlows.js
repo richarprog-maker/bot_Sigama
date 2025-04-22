9845f175bbb8af7ea5f8b9f09bb420ac8cd4e88e
richarprog-maker <quispe@novaly.com.pe>
2025-04-22 15:06:58 -0500

    mejoras

--- src/model/conversationFlows.js
+++ src/model/conversationFlows.js
@@ -65,7 +65,7 @@
         /* OT específica */
         'Para OT ESPECÍFICA, determina si se envió PLACA (6 caracteres) o NÚMERO si es distintoa 6 caraceres. ya no preguntes que te especifique',
         'Si falta placa o número, solicítalo.',
-        'si ya ingreso ot con placa o numero es especifica, por ejemplo Quiero consultar la OT 23277<> esto es una especifica y cumple todo',
+        'si ya ingreso ot con placa o numero es especifica, por ejemplo Quiero consultar la OT 23277<> esto es una especifica y cumple todo y de la misma forma con la placa por ejemplo: Quiero consultar la información de la placa BWL026',
 
         /* OT general */
         'Para OT GENERAL, pregunta qué información concreta necesita (estado, OTs abiertas, etc.) antes de generar JSON.',
